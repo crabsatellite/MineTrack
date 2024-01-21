1eed700493cd76a70e69989674b87e309f3baaed
Alex Lee <islicy@outlook.com>
2024-01-21 16:06:03 -0500

    Change comment to English

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 from pydub import AudioSegment
 import os
 
-# 获取当前脚本文件的目录
+# Get the current directory
 current_directory = os.path.dirname(os.path.realpath(__file__))
 
-# 构建到 input 和 output 文件夹的路径
+# Define input and output folders
 input_folder = os.path.join(current_directory, 'input')
 output_folder = os.path.join(current_directory, 'output')
 
 sample_rate = 44100
 
-# 创建 output 文件夹，如果它不存在
+# Create output folder if it doesn't exist
 if not os.path.exists(output_folder):
     os.makedirs(output_folder)
 
-# 遍历 input 文件夹中的所有文件
+# Iterate through all the files in the input folder
 for file_name in os.listdir(input_folder):
     if file_name.endswith(('.mp3', '.wav', '.flac')):
         file_path = os.path.join(input_folder, file_name)
@@ -22,10 +22,10 @@ for file_name in os.listdir(input_folder):
         output_file_path = os.path.join(output_folder, output_file_name)
 
         audio = AudioSegment.from_file(file_path)
-        mono_audio = audio.set_channels(1)  # 转换为单轨
-        mono_audio = mono_audio.set_frame_rate(sample_rate)  # 设置采样率
+        mono_audio = audio.set_channels(1)  # Mono channel
+        mono_audio = mono_audio.set_frame_rate(sample_rate)  # Sample rate
 
-        # 导出为 OGG 格式
+        # Export the file
         mono_audio.export(output_file_path, format='ogg')
 
 print("Conversion completed!")
