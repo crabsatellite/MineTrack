eae2eb76e4f5524f670a0837dc40731060424590
Alex Lee <islicy@outlook.com>
2024-02-05 09:53:50 -0500

    Add volume settings

--- main.py
+++ main.py
@@ -25,16 +25,19 @@ for file_name in os.listdir(input_folder):
         mono_audio = audio.set_channels(1)  # Mono channel
         mono_audio = mono_audio.set_frame_rate(sample_rate)  # Sample rate
 
+        # Decrease/Increase the volume, in dB. Negative values decrease the volume, positive values increase it.
+        decreased_volume_audio = mono_audio.apply_gain(0.0)
+
         # Check if the audio is longer than 10 minutes
-        if len(mono_audio) > 10 * 60 * 1000:
+        if len(decreased_volume_audio) > 10 * 60 * 1000:
             # Cut the audio to 10 minutes
-            mono_audio = mono_audio[:10 * 60 * 1000]
+            decreased_volume_audio = decreased_volume_audio[:10 * 60 * 1000]
 
             # Create a fade out effect for the last 30 seconds
-            fade_duration = 5 * 1000  # 5 seconds
-            mono_audio = mono_audio.fade_out(fade_duration)
+            fade_duration = 5 * 1000  # 5 seconds, adjusted to your needs
+            decreased_volume_audio = decreased_volume_audio.fade_out(fade_duration)
 
         # Export the file
-        mono_audio.export(output_file_path, format='ogg')
+        decreased_volume_audio.export(output_file_path, format='ogg')
 
 print("Conversion completed!")
